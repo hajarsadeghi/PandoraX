b1ca5e665b4d3c1de03c6f2803eb516d37f09efd
hajar <hajar@arianatech.ir>
2019-07-09 19:40:52 +0430

    sidebar menu changed based on bilono standard color and design

--- dev/js/index.js
+++ dev/js/index.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
+    // ... toggle active link in sidebar ...
+    $('.nav-item').click(function() {
+        $('nav .nav-item').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('.bilino-navbar').click(function() {
+        $(this).find('.material-icons').toggleClass('toggle');
+    });
+
+    // ... responsive sidebar ...
     $('.menu-collapse-btn').click(function() {
         $(this).toggleClass('navbar-is-visible');
         $('.bilino-navbar').toggleClass('bilino-navbar-collapse');
