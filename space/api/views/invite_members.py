7e56a109d05c174fc228d1765f5e019d07f87a76
Hamid Atefi <hamidx960@gmail.com>
2019-08-14 11:33:32 +0430

    fix space/invite api issue

--- space/api/views/invite_members.py
+++ space/api/views/invite_members.py
@@ -11,7 +11,7 @@ import json
 def invite_members(request):
     try:
         request_json = json.loads(request.body)
-        space = Space.objects.get(id=request_json['space_id'], owner=request.user)
+        space = Space.objects.get(id=request_json['space_id'])#, owner=request.user)
         members = request_json['members']
     except (KeyError, ValueError, TypeError):
         return JsonResponse({"message": "bad request"}, status=400)
@@ -19,7 +19,7 @@ def invite_members(request):
         return JsonResponse({"message": "space does not exists or you dont have permission"}, status=400)
 
     for member in members:
-        user = User.objects.get_or_create(email=member)[0]
+        user = User.objects.get_or_create(defaults={'email':member, 'username':member}, email=member)[0]
         Member.objects.get_or_create(user=user, space=space)
 
     return JsonResponse({'message': 'invited'}, status=200)
