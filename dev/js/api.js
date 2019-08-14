40c5adccab12c0550696eba342daa7aa01ef03e2
ashkan <ashkan.d@arianatech.ir>
2019-08-14 15:48:31 +0430

    merge

--- dev/js/api.js
+++ dev/js/api.js
@@ -1,17 +1,17 @@
 export const verify_email = (url, params, callback) => {
     return fetch(
-                url,
-                {
-                    method: "POST",
-                    body: JSON.stringify(params)
-                }
-            ).then(response => {
-                if (response.status >= 200 && response.status <= 299) {
-                    response.json().then(res => {
-                        callback(true, res);
-                    });
-                } else {
-                    callback(false, res);
-                }
+        url,
+        {
+            method: "POST",
+            body: JSON.stringify(params)
+        }
+    ).then(response => {
+        if (response.status >= 200 && response.status <= 299) {
+            response.json().then(res => {
+                callback(true, res);
             });
+        } else {
+            callback(false, res);
+        }
+    });
 }
