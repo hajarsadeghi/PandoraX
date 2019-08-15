2a0526fb5e5557fa7672df53cb13ecf7bd6f5cba
Hamid Atefi <hamidx960@gmail.com>
2019-08-15 20:52:25 +0430

    spaces changed to context

--- frontview/views/find_space.py
+++ frontview/views/find_space.py
@@ -3,5 +3,6 @@ from space.models import Industry
 
 def find_space(request):
     context = {}
-    context['indutries'] = Industry.objects.filter(active=True).order_by('name').values('id', 'name')
+    user_spaces = request.user.member_set.values('space__name', 'space__slug', 'space__id')
+    context['spaces'] = [{"id":space['space__id'], "name":space['space__name'], "slug":space['space__slug']} for space in user_spaces]
     return render(request, 'get_started/find_space/index.html', context=context)
