860da7c7baa06ee9764970a2112f462805deb4ad
Hamid Atefi <hamidx960@gmail.com>
2019-08-15 20:36:05 +0430

    fix find space api issue

--- space/api/views/find_space.py
+++ space/api/views/find_space.py
@@ -9,4 +9,4 @@ import json
 def find_space(request):
     user_spaces = request.user.member_set.values('space__name', 'space__slug', 'space__id')
     res = [{"id":space['space__id'], "name":space['space__name'], "slug":space['space__slug']} for space in user_spaces]
-    return JsonResponse(res, status=200)
+    return JsonResponse(res, safe=False, status=200)
