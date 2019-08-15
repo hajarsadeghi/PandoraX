59d35311238c1afc4af76dfe7a77a0b6ef93c09a
Javad Hosseini <javad@arianatech.ir>
2019-08-15 20:46:58 +0430

    find space page done

--- frontview/views/find_space.py
+++ frontview/views/find_space.py
@@ -4,4 +4,4 @@ from space.models import Industry
 def find_space(request):
     context = {}
     context['indutries'] = Industry.objects.filter(active=True).order_by('name').values('id', 'name')
-    return render(request, 'getStarted/find_space.html', context=context)
+    return render(request, 'get_started/find_space/index.html', context=context)
