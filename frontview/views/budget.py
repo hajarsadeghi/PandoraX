cec5a9141971afb04b7cf7f0d7af44d964a2eaf0
Hamid Atefi <hamidx960@gmail.com>
2019-08-17 15:07:19 +0430

    space list added

--- frontview/views/budget.py
+++ frontview/views/budget.py
@@ -6,4 +6,6 @@ from decorators import get_space
 @login_required
 @get_space
 def budget(request):
-    return render(request, 'budget/index.html', context={"space":request.space})
+    user_spaces = request.user.member_set.values('space__name', 'space__slug', 'space__id')
+    space_list = [{"id":space['space__id'], "name":space['space__name'], "slug":space['space__slug']} for space in user_spaces]
+    return render(request, 'budget/index.html', context={"space":request.space, "space_list":space_list})
