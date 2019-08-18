a3938f3e3f2e503e9d2089a063206cd5255e161d
hajar <hajar@arianatech.ir>
2019-08-18 18:59:12 +0430

    emojionearea integrated with post

--- dev/js/dashboard/feed/main.js
+++ dev/js/dashboard/feed/main.js
@@ -1,5 +1,21 @@
-$('#postToggled').on('click', () => {
-    $('.tinted').toggleClass('d-block');
-    // $(this).find('#postLink').addClass('d-none');
-    // $('#postInput').removeClass('d-none');
+$(document).ready(function() {
+    // ... initialize emoji
+    $("#postContent").emojioneArea();
+    $("#postContent").data("emojioneArea").enable();
+
+    $('#postToggled').on('click', () => {
+        if (!$('#postToggled').attr('area-expanded')) {
+            $('#postToggled').attr('aria-expanded', 'true');
+            $('#postExpanded').collapse('show');
+            $('.tinted').css({'visibility':'visible', 'opacity': '1'});
+        }
+    });
+
+    $('.tinted').on('click', () => {
+        if ($('#postToggled').attr('aria-expanded')) {
+            $('#postToggled').attr('aria-expanded', 'false');
+            $('#postExpanded').collapse('hide');
+            $('.tinted').css({'visibility':'hidden', 'opacity': '0'});
+        }
+    })
 });
