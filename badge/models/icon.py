07b23d8243df72de26181e2e25ae4dcdc10998e6
Hajar Sadeghi <hajar@arianatech.ir>
2019-09-22 17:01:59 +0330

    icon list updated

--- badge/models/icon.py
+++ badge/models/icon.py
@@ -4,7 +4,7 @@ class Icon(models.Model):
     space = models.ForeignKey('space.Space', on_delete=models.CASCADE, null=True, blank=True)
     creator = models.ForeignKey('user.User', on_delete=models.CASCADE, null=True, blank=True)
     is_global = models.BooleanField(default=False)
-    image = models.ImageField(upload_to='badge_icons/')
+    image = models.FileField(upload_to='badge_icons/')
     active = models.BooleanField(default=True)
     created_date = models.DateTimeField(auto_now_add=True, auto_now=False)
 
