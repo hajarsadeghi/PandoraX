3eb18c6ff2abae67d232c946d7583ac1f396240b
Hamid Atefi <hamidx960@gmail.com>
2019-09-28 14:36:14 +0330

    add transaction log model

--- space/models/member.py
+++ space/models/member.py
@@ -3,6 +3,10 @@ from django.db import models
 class Member(models.Model):
     space = models.ForeignKey('space.Space', on_delete=models.CASCADE)
     user = models.ForeignKey('user.User', on_delete=models.CASCADE)
+    active = models.BooleanField(default=True)
+
+    budget_point_amount = models.PositiveIntegerField(default=0)
+    earned_point_amount = models.PositiveIntegerField(default=0)
 
     class Meta:
         app_label = 'space'
