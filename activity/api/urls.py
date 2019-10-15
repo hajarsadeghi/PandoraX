fa408acdf8b66f37aaef1426cfb81057ff03f6e6
Hamid Atefi <hamidx960@gmail.com>
2019-10-15 17:03:35 +0330

    get_likers api with pagination added

--- activity/api/urls.py
+++ activity/api/urls.py
@@ -4,6 +4,7 @@ from activity.api import views
 
 urlpatterns = [
     path('<int:activity_id>/like_toggle/', views.like_toggle, name='like_toggle'),
+    path('<int:activity_id>/likers/', views.get_likers, name='get_likers'),
     path('upload_media/', views.upload_media, name='upload_media'),
     path('new/', views.new_activity, name='new_activity'),
     path('feed/', views.feed, name='feed'),
--- activity/api/views/__init__.py
+++ activity/api/views/__init__.py
@@ -2,3 +2,4 @@ from .new_activity import new_activity
 from .upload_media import upload_media
 from .feed import feed
 from .like_toggle import like_toggle
+from .get_likers import get_likers
