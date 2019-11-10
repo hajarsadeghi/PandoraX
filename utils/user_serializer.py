a31faf4746fa6e65dd38bcc3fbc2ea66e9fdc037
Hamid Atefi <hamidx960@gmail.com>
2019-11-10 15:01:40 +0330

    fix user serializer url issue

--- utils/user_serializer.py
+++ utils/user_serializer.py
@@ -8,5 +8,5 @@ def user_serializer(id, space, first_name, last_name, profile_picture):
         'full_name': get_full_name(first_name, last_name),
         'name_chars': get_name_chars(first_name, last_name),
         'profile_picture': get_media_url(profile_picture),
-        'url': reverse('frontview:dashboard:profile', args=[space.id, id])
+        'url': reverse('frontview:dashboard:profile', args=[space.slug, id])
     }
