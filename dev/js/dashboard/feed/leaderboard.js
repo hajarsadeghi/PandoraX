ce1e333f1aef3ec3ff8bfb32f9e482688a664350
hajar <hajar@arianatech.ir>
2019-11-13 15:17:14 +0330

    issues fixed

--- dev/js/dashboard/feed/leaderboard.js
+++ dev/js/dashboard/feed/leaderboard.js
@@ -71,20 +71,31 @@ function cardBoards(index, user) {
 }
 
 export function topRecords(records) {
+    
     // ... my rank
     $('.top-u-container').find('.top-u-rank').text('#' + records.my_rank);
 
     let others = records.others;
     //... number one
     others[0].profile_picture ? 
-    $('.top-three-container').find('.top-one-img').attr('src', others[0].profile_picture) : 
-    $('.top-three-container').find('.top-one-name').text(others[0].name_chars)
+    $('.top-three-container').find('.top-1').html(
+        '<div class="d-inline-block position-relative mx-3 mx-lg-0">' +
+            '<img class="img-fluid crown" src="'+ crown_img +'" alt="number one" />' +
+            '<img class="img-fluid top top-one top-one-img" src="'+ others[0].profile_picture +'" alt="profile picture" />' +
+        '<div>'
+        ) : 
+    $('.top-three-container').find('.top-1').html(
+        '<div class="d-inline-block position-relative mx-3 mx-lg-0">' +
+            '<img class="img-fluid crown" src="'+ crown_img +'" alt="number one" />' +
+            '<div class="top top-one top-one-name border d-inline-block">'+ others[0].name_chars +'</div>' +
+        '</div>'
+        )
     // ... number two
     others[1].profile_picture ? 
-    $('.top-three-container').find('.top-two-img').attr('src', others[1].profile_picture) : 
-    $('.top-three-container').find('.top-two-name').text(others[1].name_chars)
+    $('.top-three-container').find('.top-2').html('<img class="img-fluid top top-two top-two-img" src="'+ others[1].profile_picture +'" alt="profile picture" />') : 
+    $('.top-three-container').find('.top-2').html('<div class="top top-two top-two-name border d-inline-block">'+ others[1].name_chars +'</div>')
     // ... number three
     others[2].profile_picture ? 
-    $('.top-three-container').find('.top-three-img').attr('src', others[2].profile_picture) : 
-    $('.top-three-container').find('.top-three-name').text(others[2].name_chars)
+    $('.top-three-container').find('.top-3').html('<img class="img-fluid top top-three top-three-img" src="'+ others[2].profile_picture +'" alt="profile picture" />') : 
+    $('.top-three-container').find('.top-3').html('<div class="top top-three top-three-name border d-inline-block">'+ others[2].name_chars +'</div>')
 }
