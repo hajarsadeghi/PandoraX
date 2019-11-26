4ef45bb22f4b6bdd0361e0ac70094b828d6dc43b
Hamid Atefi <hamidx960@gmail.com>
2019-11-26 17:46:05 +0330

    add search to user management get user list, BIL-162

--- user/api/views/user.py
+++ user/api/views/user.py
@@ -2,7 +2,7 @@ from django.views import View
 from django.http import JsonResponse
 from decorators import is_authenticated, get_space
 from django.utils.decorators import method_decorator
-from django.db.models import Q, Sum
+from django.db.models import Q, Sum, Concat, V
 from utils import user_serializer
 import json
 
@@ -16,17 +16,44 @@ class User(View):
             'earned_point_amount': 'earned_point_amount',
             'budget_point_amount': 'budget_point_amount',
         }
+
         try:
             sort_key = request.GET.get('sort', '')
             sort_key = f"{'-' if (sort_key.replace('-', '') in sort_map and sort_key.find('-') == 0) else ''}{sort_map.get(sort_key.replace('-', ''), '-id')}"
+            search_email = request.GET.get('email')
+            search_full_name = request.GET.get('full_name')
         except (KeyError, ValueError, TypeError):
             return JsonResponse({"message": "bad params"}, status=400)
+
         res = {}
-        queryset = request.space.member_set.all().annotate(
-            earned_point_amount=Sum('user__wallet__point_amount', filter=Q(user__wallet__space=request.space, user__wallet__type=Wallet.EARNED_TYPE), distict=True),
-            budget_point_amoun=Sum('user__wallet__point_amount', filter=Q(user__wallet__space=request.space, user__wallet__type=Wallet.BUDGET_TYPE), distict=True),
-        )
-        queryset = queryset.values('user__id','user__first_name', 'user__last_name', 'user__profile_picture', 'job_title', 'active', 'user__email', 'earned_point_amount', 'budget_point_amount').order_by(sort_key)
+        filters = []
+        annotates = {
+            'earned_point_amount' : Sum('user__wallet__point_amount', filter=Q(user__wallet__space=request.space, user__wallet__type=Wallet.EARNED_TYPE), distict=True),
+            'budget_point_amoun' : Sum('user__wallet__point_amount', filter=Q(user__wallet__space=request.space, user__wallet__type=Wallet.BUDGET_TYPE), distict=True),
+        }
+
+        if search_full_name:
+            annotates['user__full_name'] = Concat('user__first_name', V(' '), 'user__last_name')
+            filters.append(Q(user__full_name__istartswith=search_full_name) | Q(user__last_name__istartswith=search_full_name))
+        if search_email:
+            filters.append(Q(user__email__istartswith=search_email))
+
+        queryset = request.space.member_set.annotate(**annotates)
+
+        if filters:
+            queryset = queryset.filter(*filters)
+
+        queryset = queryset.values(
+            'user__id',
+            'user__first_name',
+            'user__last_name',
+            'user__profile_picture',
+            'job_title',
+            'active',
+            'user__email',
+            'earned_point_amount',
+            'budget_point_amount'
+        ).order_by(sort_key).distinct()
 
         res['data'] = []
         for member in queryset:
