2df82a90c5cd1e6b0348d0d19f29ff0d1e2c2f2d
Hamid Atefi <hamidx960@gmail.com>
2019-11-27 17:46:24 +0330

    add active to team model

--- team/models/team.py
+++ team/models/team.py
@@ -6,6 +6,7 @@ class Team(models.Model):
     team_lead = models.ForeignKey('user.User', related_name='sub_team', on_delete=models.CASCADE, null=True, blank=True)
     space = models.ForeignKey('space.Space', on_delete=models.CASCADE)
     members = models.ManyToManyField('user.User', related_name='team')
+    active = models.BooleanField(default=True)
 
     class Meta:
         app_label = 'team'
