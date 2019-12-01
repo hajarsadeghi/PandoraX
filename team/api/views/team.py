375acf66c8610d9ec88d811d272a210f8f8ea5b1
Hamid Atefi <hamidx960@gmail.com>
2019-12-01 12:16:58 +0330

    get team list api added, BIL-171

--- team/api/views/team.py
+++ team/api/views/team.py
@@ -4,12 +4,55 @@ from decorators import is_authenticated, get_space
 from django.utils.decorators import method_decorator
 from team.models import Team as TeamModel
 from space.models import Member
+from django.db.models import Q, Count
+from utils import user_serializer
 import json
 
 
 @method_decorator(is_authenticated, name='dispatch')
 @method_decorator(get_space, name='dispatch')
 class Team(View):
+    def get(self, request):
+        res = {}
+        filters = [Q(space=request.space)]
+        annotates = {
+            'members_count' : Count('members', distict=True),
+        }
+        queryset = TeamModel.objects.annotate(**annotates)
+
+        if filters:
+            queryset = queryset.filter(*filters)
+
+        queryset = queryset.values(
+            'id',
+            'name',
+            'active',
+            'members_count',
+            'team_lead__id',
+            'team_lead__first_name',
+            'team_lead__last_name',
+            'team_lead__profile_picture',
+        )
+
+        res['data'] = []
+        for team in queryset:
+            tmp_team = {
+                'id': team['id'],
+                'name': team['name'],
+                'active': team['active'],
+                'members_count': team['members_count'],
+                'team_lead': user_serializer(
+                    member['team_lead__id'],
+                    request.space,
+                    member['team_lead__first_name'],
+                    member['team_lead__last_name'],
+                    member['team_lead__profile_picture'],
+                )
+            }
+            res['data'].append(tmp_team)
+        return JsonResponse(res, status=200, safe=False)
+
+
     def post(self, request):
         "create team"
         try:
--- utils/user_serializer.py
+++ utils/user_serializer.py
@@ -2,12 +2,13 @@ from utils import get_media_url, get_full_name, get_name_chars
 from django.urls import reverse
 
 
-def user_serializer(id, space, first_name, last_name, profile_picture, job_title):
-    return {
+def user_serializer(id, space, first_name, last_name, profile_picture, job_title=False):
+    tmp_user = {
         'id': id,
         'full_name': get_full_name(first_name, last_name),
         'name_chars': get_name_chars(first_name, last_name),
         'profile_picture': get_media_url(profile_picture),
-        'job_title': job_title,
         'url': reverse('frontview:dashboard:profile', args=[space.slug, id])
     }
+    if job_title != False: tmp_user['job_title'] = job_title
+    return tmp_user
