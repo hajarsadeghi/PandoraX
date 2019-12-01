e507d45381933b11defe9134ead7775faf3c1293
Hamid Atefi <hamidx960@gmail.com>
2019-12-01 11:35:32 +0330

    edit user api added, BIL-166

--- user/api/views/user_object.py
+++ user/api/views/user_object.py
@@ -46,3 +46,38 @@ class UserObject(View):
         res['bio'] = user['user__bio']
         res['teams'] = teams
         return JsonResponse(res, status=200)
+
+
+    def put(self, request, user_id):
+        "edit user"
+        member_update_fields = []
+        modify_teams = False
+
+        try:
+            member = Member.objects.get(user_id=user_id, space=request.space)
+            request_json = json.loads(request.body)
+            if 'job_title' in request_json:
+                member.job_title = request_json['job_title']
+                member_update_fields.append('job_title')
+            if 'teams' in request_json:
+                new_teams = Team.objects.filter(id__in=request_json['members'], space=request.space, active=True).values_list('id', flat=True)
+                modify_teams = True
+        except (KeyError, ValueError, TypeError):
+            return JsonResponse({"message": "bad json"}, status=400)
+        except Member.DoesNotExist:
+            return JsonResponse({"message": "team lead does not exists"}, status=400)
+
+        if member_update_fields:
+            member.save(update_fields=member_update_fields)
+
+        if modify_teams:
+            team_member_through_model = Team.members.through
+            old_teams = team_member_through_model.objects.filter(member_id=member.id).values_list('team_id', flat=True)
+            add_teams = list(set(new_teams) - set(old_teams))
+            remove_teams = list(set(old_teams) - set(new_teams))
+            team_member_through_model.objects.filter(member_id=member.id, team_id__in=remove_teams).delete()
+            new_team_objs = [team_member_through_model(member_id=member.id, team_id=team_id) for team_id in add_teams]
+            if new_team_objs:
+                team_member_through_model.objects.bulk_create(new_team_objs)
+
+        return JsonResponse({'message': 'updated'}, status=200)
