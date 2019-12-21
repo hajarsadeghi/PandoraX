230540f7422f0a84230bb539be7c80a8bef81019
Hajar Sadeghi <hajar@arianatech.ir>
2019-12-21 14:21:24 +0330

    add employees bulk add form download template, BIL-130

--- dev/js/helper/spinnerButton.js
+++ dev/js/helper/spinnerButton.js
@@ -4,7 +4,7 @@ import '../../css/helper/bilono-spinner.scss';
 $('.bilono-spinner').html( '<span>'+ $('.bilono-spinner').text() +'</span>' + '<img class="bilono-lottie mx-1 d-none" id="bilonoSpinner" src="'+ button_spinner +'" /> ')
 
 export function inprogressButton($button) {
-  let fixedWidth = Number($
+  let fixedWidth = Number($button.innerWidth()) + 33;
   $button.animate({
     'width': fixedWidth + 'px'
   }, 500, function() {
